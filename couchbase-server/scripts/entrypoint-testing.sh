1a853a3c2f1d420898ee13ac2514d8497d23e60c
Kenneth Lareau <kenneth.lareau@couchbase.com>
2018-01-10 15:27:01 -0800

    Added Dockerfile and entrypoint script to support SSH for testing

---
+++ couchbase-server/scripts/entrypoint-testing.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+/usr/sbin/sshd -D &
+
+[[ "$1" == "couchbase-server" ]] && {
+    echo "Starting Couchbase Server -- Web UI available at http://<ip>:8091 and logs available in /opt/couchbase/var/lib/couchbase/logs"
+    exec /sbin/runsvdir-start
+}
+
+exec "$@"
