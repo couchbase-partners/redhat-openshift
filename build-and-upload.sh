0a9abb49d99d989c359e1ef32a1b2ebc90d9ae58
Kenneth Lareau <kenneth.lareau@couchbase.com>
2018-09-05 14:08:29 -0700

    Add a 'staging' option to OpenShift image creation

--- build-and-upload.sh
+++ build-and-upload.sh
@@ -7,6 +7,7 @@ function show_help {
     echo "  -p : Product to build and upload from (e.g. couchbase-server) (Required)"
     echo "  -v : Version of product to use (e.g. 5.1.0) (Required)"
     echo "  -b : Build number of image (Optional, defaults to 1)"
+    echo "  -s : Build staging test image and tarball (Boolean)"
     echo "  -t : Build testing image and tarball (Boolean)"
     exit 0
 }
@@ -19,6 +20,8 @@ while getopts :p:v:b:th opt; do
         v) VERSION="$OPTARG"
            ;;
         b) BUILD="$OPTARG"
+           ;;
+        s) STAGING="true"
            ;;
         t) TESTING="true"
            ;;
@@ -50,12 +53,13 @@ cd ${PRODUCT}
 # Check for testing build and set up variables accordingly
 DOCKER_FILE=Dockerfile
 NAME_EXT=""
+S3_STAGING=""
 
-if [[ "$TESTING" == "true" ]]; then
+if [[ "$STAGING" == "true" || "$TESTING" == "true" ]]; then
    NAME_EXT="-testing"
 
-   if [[ -e "Dockerfile.testing" ]]; then
-       DOCKER_FILE=Dockerfile.testing
+   if [[ "$STAGING" == "true" ]]; then
+       S3_STAGING="-staging"
    fi
 fi
 
@@ -67,10 +71,10 @@ UPLOAD_HOST=scan.connect.redhat.com
 UPLOAD_URI=${UPLOAD_HOST}/${PROJECT_ID}/${IMAGE}:${VERSION}-${BUILD}
 
 # Build image, acquiring image ID (needed for upload)
-IMAGE_ID=$(docker build --build-arg PROD_VERSION=${VERSION} --build-arg OS_BUILD=${BUILD} -f ${DOCKER_FILE} -t ${IMAGE} . 2>/dev/null | awk '/Successfully built/{print $NF}')
+IMAGE_ID=$(docker build --build-arg PROD_VERSION=${VERSION} --build-arg OS_BUILD=${BUILD} --build-arg STAGING=${S3_STAGING} -f ${DOCKER_FILE} -t ${IMAGE} . 2>/dev/null | awk '/Successfully built/{print $NF}')
 
 # If testing, create tarball of image, else tag and upload image to OpenShift
-if [[ "$TESTING" == "true" ]]; then
+if [[ "$STAGING" == "true" || "$TESTING" == "true" ]]; then
     docker save -o ${IMAGE}.tar ${IMAGE}
 else
     docker login -u unused -p "$(cat ${CONF_DIR}/registry_key)" -e none ${UPLOAD_HOST}
